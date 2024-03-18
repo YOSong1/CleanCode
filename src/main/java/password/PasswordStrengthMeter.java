6d45966512f1bcb16bd083998191e7fe8eef9fda
yysong1206 <yysong1206@gmail.com>
2024-03-18 22:18:45 +0900

    6단계 테스트 : 길이가 8글자 이상인 조건만 충족하는 경우

--- src/main/java/password/PasswordStrengthMeter.java
+++ src/main/java/password/PasswordStrengthMeter.java
@@ -12,6 +12,8 @@ public class PasswordStrengthMeter {
 
         if(lengthEnough && !containsNum && !containsUpp)
             return PasswordStrength.WEAK;
+        if(!lengthEnough && containsNum && !containsUpp)
+            return PasswordStrength.WEAK;
 
         if(!lengthEnough) return PasswordStrength.NORMAL;
         if(!containsNum) return PasswordStrength.NORMAL;
