747ca53dfbc86b699f46a4880faf65746db486fc
yysong1206 <yysong1206@gmail.com>
2024-03-18 22:30:10 +0900

    8단계까지 Refactoring

--- src/main/java/password/PasswordStrengthMeter.java
+++ src/main/java/password/PasswordStrengthMeter.java
@@ -3,23 +3,18 @@ package main.java.password;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s){
         if(s==null || s.isEmpty()) return PasswordStrength.INVALID;
-//        if(s.length() < 8){
-//            return PasswordStrength.NORMAL;
-//        }
+        int metCounts = 0;
         boolean lengthEnough = s.length() >= 8;
+        if(lengthEnough) metCounts++;
+
         boolean containsNum = meetsContainingNumberCriteria(s);
+        if(containsNum) metCounts++;
+
         boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        if(containsUpp) metCounts++;
 
-        if(lengthEnough && !containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if(!lengthEnough && containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        if(!lengthEnough && !containsNum && containsUpp)
-            return PasswordStrength.WEAK;
-
-        if(!lengthEnough) return PasswordStrength.NORMAL;
-        if(!containsNum) return PasswordStrength.NORMAL;
-        if(!containsUpp) return PasswordStrength.NORMAL;
+        if(metCounts == 1) return PasswordStrength.WEAK;
+        if(metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
